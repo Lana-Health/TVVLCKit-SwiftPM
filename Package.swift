7e20f592d4efaf3f1a635994fde90d60791cd95c
apoorva <apoorva.chandrakar@lana.health>
2026-02-09 16:09:57 +0530

    chore: update to v3.6.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TVVLCKitTarget",
-            url: "https://github.com/Lana-Health/TVVLCKit-SPM/releases/download/v3.7.2/TVVLCKit.xcframework.zip",
-            checksum: "37bc6a8198f96ccd06aebc5ee878eeacc8c40a81c47bb8c67a8f50ec40dbf95d"
+            url: "https://github.com/Lana-Health/TVVLCKit-SPM/releases/download/v3.6.0/TVVLCKit.xcframework.zip",
+            checksum: "7afd8f0b6e53c73ce164aa5551827468f134e0e7c304b5b08fb3d14860785935"
         )
     ]
 )
