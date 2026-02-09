4a3ef56f9abd1d8021a93ea464505bc6fd39a2ba
apoorva <apoorva.chandrakar@lana.health>
2026-02-09 15:28:03 +0530

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TVVLCKit-SPM",
+    platforms: [
+        .tvOS(.v13) // Set this to your minimum requirement
+    ],
+    products: [
+        .library(
+            name: "TVVLCKit",
+            targets: ["TVVLCKitTarget"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TVVLCKitTarget",
+            url: "https://github.com/Lana-Health/TVVLCKit-SPM/releases/download/v3.7.2/TVVLCKit.xcframework.zip",
+            checksum: "37bc6a8198f96ccd06aebc5ee878eeacc8c40a81c47bb8c67a8f50ec40dbf95d"
+        )
+    ]
+)
